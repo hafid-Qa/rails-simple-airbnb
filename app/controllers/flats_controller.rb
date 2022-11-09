8b57b76d03e4a057a2134ae17f58092b5227ded0
Hafid Qarchi <haqarchi@gmail.com>
2022-11-09 15:30:13 +0900

    created index view and stylied nav bar

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,25 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  # def show
+  #   @flat = flat.find(params[:id])
+  # end
+
+  # def create
+  #   @flat = flat.new(params[:flat])
+  # end
+
+  # def update
+  #   @flat = flat.find(params[:id])
+  # end
+
+  # def edit
+  #   @flat = flat.find(params[:id])
+  # end
+
+  # def destroy
+  #   flats = flat.find(params[:id])
+  # end
 end
